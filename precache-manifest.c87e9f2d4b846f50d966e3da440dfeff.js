6cb7334059c81809939f3789a22584f940292ea7
Damla Koksal <damlakoksal@outlook.com>
2020-09-26 13:08:02 +0300

    Updates

---
+++ precache-manifest.c87e9f2d4b846f50d966e3da440dfeff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4b6d71f85b4d59fd4c992ab2ce4c1e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3140563a24275eb0c52",
+    "url": "/static/css/main.3a9285b5.chunk.css"
+  },
+  {
+    "revision": "27fbf84c6d7970776b10",
+    "url": "/static/js/2.fe286153.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe286153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3140563a24275eb0c52",
+    "url": "/static/js/main.f152874a.chunk.js"
+  },
+  {
+    "revision": "de0ab54ff28461e16cb3",
+    "url": "/static/js/runtime-main.ca4dc10d.js"
+  }
+]);
