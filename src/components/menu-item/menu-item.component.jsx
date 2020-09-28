ddf0a78d81c1da1dd18e193b47b661f89704cf2c
Damla Koksal <damlakoksal@outlook.com>
2020-09-28 23:53:11 +0300

    Transition added to menu-item components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import './menu-item.style.scss';
+import "./menu-item.style.scss";
 
-const MenuItem = ( { title, imageUrl, size } ) => (
-    <div style={{ backgroundImage: `url(${ imageUrl })` }} className={`${ size } menu-item`}>
-       <div className="content">
-           <h1 className="title">
-               {title}
-           </h1>
-           <span className="subtitle">
-                SHOP NOW
-           </span>
-       </div>
-    </div>
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div className={`${size} menu-item`}>
+    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }}></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+  </div>
 );
 
 export default MenuItem;
