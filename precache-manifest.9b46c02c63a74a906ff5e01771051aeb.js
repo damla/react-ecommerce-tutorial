c66c43e1ac92d1ff182e9525de6d4614c719c9cc
Damla Koksal <damlakoksal@outlook.com>
2020-10-29 17:16:17 +0300

    Updates

---
+++ precache-manifest.9b46c02c63a74a906ff5e01771051aeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55019f77c2b62d2219a13cbd54f39dc7",
+    "url": "/react-ecommerce-tutorial/index.html"
+  },
+  {
+    "revision": "d72e4c85ef83ed009b69",
+    "url": "/react-ecommerce-tutorial/static/css/main.d6a565e8.chunk.css"
+  },
+  {
+    "revision": "f1faf95fcd177fddccc3",
+    "url": "/react-ecommerce-tutorial/static/js/2.50c117d8.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/react-ecommerce-tutorial/static/js/2.50c117d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72e4c85ef83ed009b69",
+    "url": "/react-ecommerce-tutorial/static/js/main.8ebbe1b7.chunk.js"
+  },
+  {
+    "revision": "94b7d3350af205c12977",
+    "url": "/react-ecommerce-tutorial/static/js/runtime-main.57390ec5.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/react-ecommerce-tutorial/static/media/crown.ed28accb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff1655aa60bbbf067a229a607de85666.js"
+  "/react-ecommerce-tutorial/precache-manifest.9b46c02c63a74a906ff5e01771051aeb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ecommerce-tutorial/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
