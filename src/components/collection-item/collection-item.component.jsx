569754024540c62839f5ca764c7caf1cb140d56b
Damla Koksal <damlakoksal@outlook.com>
2020-11-02 14:30:03 +0300

    AddItem action added to redux.

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,22 +1,35 @@
 import React from "react";
+import { connect } from "react-redux";
 
+import CustomButton from "../custom-button/custom-button.component";
+
+import { addItem } from "../../redux/cart/cart.actions";
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
+
   return (
     <div className="collection-item">
       <div
         className="image"
         style={{
-          backgroundImage: `url(${imageUrl})`
+          backgroundImage: `url(${imageUrl})`,
         }}
       />
-        <div className="collection-footer">
-            <span className='name'>{ name }</span>
-            <span className='price'>{ price }</span>
-        </div>
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        ADD TO CART
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)), // ekle dendiginde bu methoda baglanmasini sagliyoruz
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
